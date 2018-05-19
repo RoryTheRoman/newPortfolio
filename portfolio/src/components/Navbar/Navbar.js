72d2d22f1e88d4b643fddb128e15bccc24f832d9
Mike D <michael.mcdaniel210@gmail.com>
2018-05-19 13:36:20 -0500

    some changes to itemCard

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -7,18 +7,18 @@ const Navbar = () => {
 return (
     <div className="container navbar-container">
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
-        <div className="navbar-brand" >Mike McDaniel</div>
+        <div className="navbar-brand" >michael mcdaniel</div>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
-                    <a className="nav-link" href="/">Works <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="/">WORKS <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
+                    <a className="nav-link" href="/about">ABOUT</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/links">Links</a>
+                    <a className="nav-link" href="/links">CONTACT</a>
                 </li>
             </ul>
   </div>
