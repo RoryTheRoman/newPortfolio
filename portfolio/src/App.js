e3aba570ccd3114724e06492bfa6084950222ed7
Mike D <michael.mcdaniel210@gmail.com>
2018-05-23 15:17:27 -0500

    began to change app.js to routes and move the component display to the pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,19 +5,30 @@ import Navbar from './components/Navbar';
 import ItemCard from './components/ItemCard';
 import FeatureItem from './components/FeatureItem';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Works from './pages/Works/Works.js';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <ItemCard />
-        <FeatureItem />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Works} />
+            <Route exact path="/Works" component={Works} />
+          </Switch>
+        </div>
+      </Router>
 
     )
   }
 }
 
 export default App;
+
+{/* <div className="App">
+  <Navbar />
+  <ItemCard />
+  <FeatureItem />
+</div> */}
---
+++ portfolio/src/pages/Works/Works.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ItemCard from '../../components/ItemCard';
+
+const Works = props => (
+
+    <ItemCard />
+);
+export default Works;
