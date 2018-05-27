ba53250b08635ede0bbc49e06b3c4d596d52e2e0
Mike D <michael.mcdaniel210@gmail.com>
2018-05-27 16:08:28 -0500

    completed about page for now

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -1,10 +1,20 @@
 import React from "react";
 import FeatureItem from '../../components/FeatureItem';
+import Sidebar from '../../components/Sidebar';
 
 const About = props => (
-    <div>
-    <FeatureItem />
-    {/* <Sidebar /> */}
+    <div className="about">
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <FeatureItem />
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <Sidebar />
+                </div>    
+            </div>
+        </div>    
     </div>
 
 );
