b9a04aebac2a5933b8e37b8644dbe175335c9d72
Mike D <michael.mcdaniel210@gmail.com>
2018-05-27 18:12:16 -0500

    moved properties to json, rendered sidebar correctly on about page

--- portfolio/src/components/Sidebar/Sidebar.js
+++ portfolio/src/components/Sidebar/Sidebar.js
@@ -6,7 +6,7 @@ const Sidebar = props => {
     return(
         <div className="sidebar">
             <div className="card sidebar-card">
-                <div className="card-body">
+                <div className="card-body" className={props.name}>
                     <h5 className="card-title">{props.title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">{props.subtitle}</h6>
                     <p className="text-left pOne">{props.pOne}</p>
--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -2,8 +2,15 @@ import React, {Component} from "react";
 import FeatureItem from '../../components/FeatureItem';
 import Sidebar from '../../components/Sidebar';
 import Navbar from '../../components/Navbar';
+import sidebar from '../../sidebar.json';
 
 class About extends Component {
+
+    state = {
+        
+        sidebar: sidebar,
+    };
+
     render() {
         return (
             <div className="about">
@@ -13,7 +20,21 @@ class About extends Component {
                             <FeatureItem />
                         </div>
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                            <Sidebar />
+                            {this.state.sidebar.map(item => (
+                                <Sidebar
+                                    key={item.id}
+                                    id={item.id}
+                                    name={item.name}
+                                    title={item.title}
+                                    subtitle={item.subtitle}
+                                    pOne={item.pOne}
+                                    pTwo={item.pTwo}
+                                    pThree={item.pThree}
+                                    pFour={item.pFour}
+                                    pFive={item.pFive}
+                                />
+                            ))}
+
                         </div>
                     </div>
                 </div>
