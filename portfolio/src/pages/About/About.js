0aa11b1a3555f46c35913c8d5530f10eeae6aa2e
Mike D <michael.mcdaniel210@gmail.com>
2018-05-27 18:31:02 -0500

    about page fully working

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -9,6 +9,10 @@ class About extends Component {
     state = {
         
         sidebar: sidebar,
+        id: sidebar[0].id,
+        image: sidebar[0].image,
+        name: sidebar[0].name,
+
     };
 
     render() {
@@ -17,7 +21,11 @@ class About extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                            <FeatureItem />
+                            <FeatureItem 
+                                key={this.state.id}
+                                id={this.state.id}
+                                image={this.state.image}
+                            />
                         </div>
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             {this.state.sidebar.map(item => (
