87ba85482f7ca5873852b47b463353f18cc94b6d
Mike D <michael.mcdaniel210@gmail.com>
2018-05-27 16:22:13 -0500

    fixed rendering on about page

--- portfolio/src/components/Sidebar/Sidebar.js
+++ portfolio/src/components/Sidebar/Sidebar.js
@@ -3,20 +3,23 @@ import "./Sidebar.css";
 
 const Sidebar = props => {
 
-    <div className="sidebar">
-        <div className="card sidebar-card">
-            <div className="card-body">
-                <h5 className="card-title">Michael McDaniel</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Full Stack Web Developer</h6>
-                <p>I'm a Chicago based freelance web developer, passionate about designing and building.  I love the satisfaction that comes with contributing to a project.
-                My entry into development came in the late 1980s.  When other kids my age were at summer camp, I was at computer camp.  Working on a TI-99, I learned to program an animated smiley face.  For a nine-year-old, this was magic!  
-                Unfortunately, for many years after that I didn't do much programming.  I messed around with HTML templates on different blog sites (shout out to diaryland.com!) but career-wise my path completely diverged.  I graduated in 2003 from Edgewood College with a BS in Psychology and a Minor in Art.  I worked in the public sector for many years, and ended up at a third-party credit reporting agency in Chicago - where I work currently.
-                When bootcamps came on the scene, I was immediately interested.  Unable to convince myself that I should quit my job and attend a full time bootcamp, I eventually found a part-time Full Stack program at Northwestern University that allowed me to work and learn.  The program focused on the MERN stack, which built on the HTML, CSS and Javascript fundamentals we were taught.  I graduated May 2018, and I am looking to transition into a full time Web Developer position.
+    return(
+        <div className="sidebar">
+            <div className="card sidebar-card">
+                <div className="card-body">
+                    <h5 className="card-title">Michael McDaniel</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Full Stack Web Developer</h6>
+                    <p>I'm a Chicago based freelance web developer, passionate about designing and building.  I love the satisfaction that comes with contributing to a project.
+                    My entry into development came in the late 1980s.  When other kids my age were at summer camp, I was at computer camp.  Working on a TI-99, I learned to program an animated smiley face.  For a nine-year-old, this was magic!
+                    Unfortunately, for many years after that I didn't do much programming.  I messed around with HTML templates on different blog sites (shout out to diaryland.com!) but career-wise my path completely diverged.  I graduated in 2003 from Edgewood College with a BS in Psychology and a Minor in Art.  I worked in the public sector for many years, and ended up at a third-party credit reporting agency in Chicago - where I work currently.
+                    When bootcamps came on the scene, I was immediately interested.  Unable to convince myself that I should quit my job and attend a full time bootcamp, I eventually found a part-time Full Stack program at Northwestern University that allowed me to work and learn.  The program focused on the MERN stack, which built on the HTML, CSS and Javascript fundamentals we were taught.  I graduated May 2018, and I am looking to transition into a full time Web Developer position.
                 Finding web development has been the greatest gift.  It is creativity with structure, and allows me to problem solve again.  Above else, I love the fact that it is a career in which you have to continue to learn and grow.  A developers education never stagnates</p>
+                </div>
             </div>
+
         </div>
-    
-    </div>
+
+    )
 
 
 
--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -1,18 +1,18 @@
 import React, {Component} from "react";
 import FeatureItem from '../../components/FeatureItem';
 import Sidebar from '../../components/Sidebar';
+import Navbar from '../../components/Navbar';
 
 class About extends Component {
     render() {
         return (
             <div className="about">
-                <Navbar />
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <FeatureItem />
                         </div>
-                        <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <Sidebar />
                         </div>
                     </div>
