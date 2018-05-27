c0e281d7459b611aaecdbd37424499fba93ad7ba
Mike D <michael.mcdaniel210@gmail.com>
2018-05-27 16:52:20 -0500

    fixed css of sidebar

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -9,10 +9,10 @@ class About extends Component {
             <div className="about">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <FeatureItem />
                         </div>
-                        <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <Sidebar />
                         </div>
                     </div>
