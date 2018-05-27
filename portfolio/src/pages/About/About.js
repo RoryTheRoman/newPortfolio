bfcc89dab2d5baacc410d3510cb8343f0866f136
Mike D <michael.mcdaniel210@gmail.com>
2018-05-27 16:13:00 -0500

    switched around about page to render about components

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import FeatureItem from '../../components/FeatureItem';
 import Sidebar from '../../components/Sidebar';
 
-const About = props => (
-    <div className="about">
-        <Navbar/>
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                    <FeatureItem />
+class About extends Component {
+    render() {
+        return (
+            <div className="about">
+                <Navbar />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                            <FeatureItem />
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <Sidebar />
+                        </div>
+                    </div>
                 </div>
-                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                    <Sidebar />
-                </div>    
             </div>
-        </div>    
-    </div>
 
-);
+        )
+    }
+
+}
 export default About;
