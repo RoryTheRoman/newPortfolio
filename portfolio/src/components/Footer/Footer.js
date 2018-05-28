3e79162a5158d47e88fdfcd95004d76e5815a72c
Mike D <michael.mcdaniel210@gmail.com>
2018-05-28 12:04:19 -0500

    some styling, footer complete

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -6,29 +6,27 @@ const Footer = () => {
 
     return (
         <footer className="footer">
-            <div className="container">
+            {/* <div className="container"> */}
                 <div className="row">
-                    <div className="col-lg-4">
+                    <div className="col-lg-6">
                         <a href="https://github.com/RoryTheRoman" target="_blank">
-                            <img src="./assets/images/github.png" alt="github"/>
+                            <img src="./assets/images/github.png" alt="github" className="socialMedia" />
                         </a>
                         <a href="https://www.linkedin.com/in/michael-mcdaniel-17b0b3aa/" target="_blank">
-                            <img src="./assets/images/linkedin.png" alt="linkedin" />
+                            <img src="./assets/images/linkedin.png" alt="linkedin" className="socialMedia" />
                         </a>
                         <a href="https://plus.google.com/u/0/105620729477638661676" target="_blank">
-                            <img src="./assets/images/googleplus.png" alt="googleplus" />
+                            <img src="./assets/images/googleplus.png" alt="googleplus" className="socialMedia" />
                         </a>
                         <a href="https://stackoverflow.com/story/rorytherroman" target="_blank">
-                            <img src="./assets/images/stackoverflow.png" alt="stackoverflow" />
+                            <img src="./assets/images/stackoverflow.png" alt="stackoverflow" className="socialMedia" />
                         </a>
                     </div>
-                    <div className="col-lg-4">
-                        <h4 className="copyright">© Michael McDaniel 2018</h4>
-                    </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-6 ml-auto">
+                        <h6 className="copyright">© Michael McDaniel 2018</h6>
                     </div>
                 </div>
-            </div>
+            {/* </div> */}
         </footer>
 
 
